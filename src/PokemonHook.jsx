399d8df9abe6f2e909eb5afa91458bc050d453f3
Alex H <alex.holder@bigfootds.com>
2023-09-26 19:45:07 +1000

    feat: usestate implemented

--- src/PokemonHook.jsx
+++ src/PokemonHook.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 
 
 export default function PokemonHook(){
 
+
+	//  [variable, function to set variable] = useState(initial data);
+	let [pokemon, setPokemon] = useState({});
+
+	let [someExampleState, setSomeExampleState] = useState("hello world");
+	
+
 	return (
 		<div>
-			<h1>Feature not yet implemented.</h1>
+			<h1>{someExampleState}</h1>
+			<h1>{pokemon.name}</h1>
 		</div>
 	)
 }
