5f6c3d1c196d5f33c3dd4bb4f7d40e5de25c3935
Alex H <alex.holder@bigfootds.com>
2023-09-26 19:35:26 +1000

    feat: project init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PokemonHook from './PokemonHook';
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonHook />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PokemonHook.jsx
@@ -0,0 +1,10 @@
+
+
+export default function PokemonHook(){
+
+	return (
+		<div>
+			<h1>Feature not yet implemented.</h1>
+		</div>
+	)
+}
